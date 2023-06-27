044be0ee49120877a39db6058aa0de5c73c5c911
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-06-27 18:02:09 +0400

    feat:add tabs

--- src/components/header/header-help-links/header-help-links.tsx
+++ src/components/header/header-help-links/header-help-links.tsx
@@ -9,74 +9,92 @@ const HeaderHelpLinks = () => {
 
   return (
     <nav className={styles.navigation}>
-      <ul
-        className={classNames(
-          "m-0",
-          "p-0",
-          "text",
-          "text_size_small ",
-          "list",
-          styles.mainContainer
-        )}
-      >
-        <div>
-          <NavLink
-            to="/b2b"
-            className={classNames(styleForLink, "link", styles.b2bContent)}
-          >
-            <li>Покупать как компания</li>
-          </NavLink>
-        </div>
-        <div className={styles.containerForContacts}>
-          <NavLink
-            to="/inspiration"
-            className={classNames(
-              styleForLink,
-              "link",
-              styles.inspirationContent
-            )}
-          >
-            <li>
-              <button
-                className={classNames(
-                  styles.inspirationContent,
-                  "text",
-                  "text_size_small "
-                )}
-              >
-                Вдохновиться
-              </button>
-            </li>
-          </NavLink>
-          <NavLink
-            to="/constructor"
-            className={classNames(styleForLink, "link", styles.contactsContent)}
-          >
-            <li>Помочь собрать книгу</li>
-          </NavLink>
-          <NavLink
-            to="/vacancy"
-            className={classNames(styleForLink, "link", styles.contactsContent)}
-          >
-            <li>Работать у нас</li>
-          </NavLink>
-          <NavLink
-            to="/faq"
-            className={classNames(styleForLink, "link", styles.contactsContent)}
-          >
-            <li className={styles.contentWithArrow}>
-              Помощь
-              <img src={arrowDown} alt="стрелка вниз" />
-            </li>
-          </NavLink>
-          <NavLink
-            to="/contacts"
-            className={classNames(styleForLink, "link", styles.contactsContent)}
-          >
-            <li>Контакты</li>
-          </NavLink>
-        </div>
-      </ul>
+      <div className={styles.navWrapper}>
+        <ul
+          className={classNames(
+            "m-0",
+            "p-0",
+            "text",
+            "text_size_small ",
+            "list",
+            styles.mainContainer
+          )}
+        >
+          <div>
+            <NavLink
+              to="/b2b"
+              className={classNames(styleForLink, "link", styles.b2bContent)}
+            >
+              <li>Покупать как компания</li>
+            </NavLink>
+          </div>
+          <div className={styles.containerForContacts}>
+            <NavLink
+              to="/inspiration"
+              className={classNames(
+                styleForLink,
+                "link",
+                styles.inspirationContent
+              )}
+            >
+              <li>
+                <button
+                  className={classNames(
+                    styles.inspirationContent,
+                    "text",
+                    "text_size_small "
+                  )}
+                >
+                  Вдохновиться
+                </button>
+              </li>
+            </NavLink>
+            <NavLink
+              to="/constructor"
+              className={classNames(
+                styleForLink,
+                "link",
+                styles.contactsContent
+              )}
+            >
+              <li>Помочь собрать книгу</li>
+            </NavLink>
+            <NavLink
+              to="/vacancy"
+              className={classNames(
+                styleForLink,
+                "link",
+                styles.contactsContent
+              )}
+            >
+              <li>Работать у нас</li>
+            </NavLink>
+            <NavLink
+              to="/faq"
+              className={classNames(
+                styleForLink,
+                "link",
+                styles.contactsContent
+              )}
+            >
+              <li className={styles.contentWithArrow}>
+                Помощь
+                <img src={arrowDown} alt="стрелка вниз" />
+              </li>
+            </NavLink>
+            <NavLink
+              to="/contacts"
+              className={classNames(
+                styleForLink,
+                "link",
+                styles.contactsContent
+              )}
+            >
+              <li>Контакты</li>
+            </NavLink>
+          </div>
+        </ul>
+      </div>
     </nav>
   );
 };
--- src/components/header/header-menu/header-menu.tsx
+++ src/components/header/header-menu/header-menu.tsx
@@ -1,140 +1,206 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../logo/logo";
 import styles from "./header-menu.module.css";
 import arrowDown from "../../app/assets/images/MediumArrowDown.svg";
 import arrowRight from "../../app/assets/images/SmallArrowRight.svg";
 import classNames from "classnames";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 const HeaderMenu = () => {
   const [isContainerHovered, setIsContainerHovered] = useState(false);
-
-  const styleForLink: any = ({ isActive, isPending }) =>
-    isPending ? styles.pending : isActive ? styles.active : "";
+  const [isDropDownMenuOpened, setIsDropDownMenuOpened] = useState(false);
 
   const onMouseOverHandler = () => {
     if (!isContainerHovered) {
       setIsContainerHovered(true);
+      setIsDropDownMenuOpened(true);
     }
+  };
+
+  const onDropDownMenuClickHandler = () => {
+    setIsDropDownMenuOpened(false);
+    setIsContainerHovered(false);
   };
 
   return (
     <>
       <div className={styles.mainContainer}>
         <Logo />
-        <ul
-          className={classNames(
-            "m-0",
-            "p-0",
-            "text",
-            "text_size_medium",
-            "list",
-            styles.itemsList
-          )}
-        >
-          <NavLink
-            to="/products/cards"
-            className={classNames(styleForLink, "link")}
+        <Tabs defaultIndex={1}>
+          <TabList
+            className={classNames(
+              "m-0",
+              "p-0",
+              "text",
+              "text_size_medium",
+              "list",
+              styles.itemsList
+            )}
           >
-            <li className={styles.menuContent}>Карточки</li>
-          </NavLink>
-          <NavLink
-            to="/products/photobooks"
-            className={classNames(styleForLink, "link")}
-          >
-            <li className={styles.menuContent}>Фотокниги</li>
-          </NavLink>
-          <NavLink
-            to="/products/certificate"
-            className={classNames(styleForLink, "link")}
-          >
-            <li className={styles.menuContent}>Сертификаты</li>
-          </NavLink>
-          <NavLink
-            to="/products/gifts"
-            className={classNames(styleForLink, "link")}
-          >
-            <li className={styles.menuContent}>В подарок</li>
-          </NavLink>
-          <li
-            className={styles.dropDownContainer}
-            onMouseOver={onMouseOverHandler}
-          >
-            <div className={styles.dropDownContentTitle}>
-              Ещё
-              <img
-                className={isContainerHovered ? styles.arrowDown : ""}
-                src={arrowDown}
-                alt="стрелка вниз"
-              />
-            </div>
-            <div className={styles.dropDownContent}>
+            <Tab
+              className={styles.tab}
+              selectedClassName={classNames(styles.selectedTab)}
+            >
               <NavLink
-                to="/products/tradition-book"
-                className={classNames(styleForLink, "link")}
+                className={classNames("link", styles.menuContent)}
+                to="/products/cards"
               >
-                <li className={styles.content}>Книга традиций</li>
+                Карточки
               </NavLink>
+            </Tab>
+            <Tab
+              className={styles.tab}
+              selectedClassName={classNames(styles.selectedTab)}
+            >
               <NavLink
-                to="/products/notebook"
-                className={classNames(styleForLink, "link")}
+                className={classNames("link", styles.menuContent)}
+                to="/products/photobooks"
               >
-                <li className={styles.dropDownContainerForNotebook}>
-                  <div className={styles.contentWithArrow}>
-                    Блокноты
-                    <img
-                      className={styles.arrowRight}
-                      src={arrowRight}
-                      alt="стрелка вправо"
-                    />
-                  </div>
-                  <div className={styles.separator}></div>
-                  <div className={styles.dropDownContentForNoteBook}>
+                Фотокниги
+              </NavLink>
+            </Tab>
+            <Tab
+              className={styles.tab}
+              selectedClassName={classNames(styles.selectedTab)}
+            >
+              <NavLink
+                className={classNames("link", styles.menuContent)}
+                to="/products/certificate"
+              >
+                Сертификаты
+              </NavLink>
+            </Tab>
+            <Tab
+              className={styles.tab}
+              selectedClassName={classNames(styles.selectedTab)}
+            >
+              <NavLink
+                className={classNames("link", styles.menuContent)}
+                to="/products/gifts"
+              >
+                В подарок
+              </NavLink>
+            </Tab>
+            <Tab className={styles.tab} selectedClassName={styles.selectedTab}>
+              <li
+                className={styles.dropDownContainer}
+                onMouseOver={onMouseOverHandler}
+              >
+                <div className={styles.dropDownContentTitle}>
+                  Ещё
+                  <img
+                    className={isContainerHovered ? styles.arrowDown : ""}
+                    src={arrowDown}
+                    alt="стрелка вниз"
+                  />
+                </div>
+                {isDropDownMenuOpened && (
+                  <div className={styles.dropDownContent}>
                     <NavLink
-                      to="/products/notebook/with-photo"
-                      className={classNames(styleForLink, "link")}
+                      to="/products/tradition-book"
+                      className={classNames("link")}
                     >
-                      <li className={styles.content}>Блокнот с фото</li>
+                      <li
+                        className={styles.content}
+                        onClick={onDropDownMenuClickHandler}
+                      >
+                        Книга традиций
+                      </li>
                     </NavLink>
                     <NavLink
-                      to="/products/notebook/ready-made"
-                      className={classNames(styleForLink, "link")}
+                      to="/products/notebook"
+                      className={classNames("link")}
                     >
-                      <li className={styles.content}>Готовые блокноты</li>
+                      <li className={styles.dropDownContainerForNotebook}>
+                        <div className={styles.contentWithArrow}>
+                          Блокноты
+                          <img
+                            className={styles.arrowRight}
+                            src={arrowRight}
+                            alt="стрелка вправо"
+                          />
+                        </div>
+                        <div className={styles.separator}></div>
+                        <div className={styles.dropDownContentForNoteBook}>
+                          <NavLink
+                            to="/products/notebook/with-photo"
+                            className={classNames("link")}
+                          >
+                            <li
+                              className={styles.content}
+                              onClick={onDropDownMenuClickHandler}
+                            >
+                              Блокнот с фото
+                            </li>
+                          </NavLink>
+                          <NavLink
+                            to="/products/notebook/ready-made"
+                            className={classNames("link")}
+                          >
+                            <li
+                              className={styles.content}
+                              onClick={onDropDownMenuClickHandler}
+                            >
+                              Готовые блокноты
+                            </li>
+                          </NavLink>
+                        </div>
+                      </li>
+                    </NavLink>
+                    <li className={styles.unactiveContent}>Кому</li>
+                    <NavLink
+                      to="/products/to-friends"
+                      className={classNames("link")}
+                    >
+                      <li
+                        className={styles.content}
+                        onClick={onDropDownMenuClickHandler}
+                      >
+                        Друзьям
+                      </li>
+                    </NavLink>
+                    <NavLink
+                      to="/products/to-relatives"
+                      className={classNames("link")}
+                    >
+                      <li className={styles.unactiveContent}>Близким</li>
+                    </NavLink>
+                    <NavLink
+                      to="/products/to-just-merried"
+                      className={classNames("link")}
+                    >
+                      <li
+                        className={styles.content}
+                        onClick={onDropDownMenuClickHandler}
+                      >
+                        Молодожёнам
+                      </li>
+                    </NavLink>
+                    <div className={styles.separator}></div>
+                    <li className={styles.unactiveContent}>...</li>
+                    <NavLink
+                      to="/products/wedding"
+                      className={classNames("link")}
+                    >
+                      <li
+                        className={styles.content}
+                        onClick={onDropDownMenuClickHandler}
+                      >
+                        Свадьба
+                      </li>
                     </NavLink>
                   </div>
-                </li>
-              </NavLink>
-              <li className={styles.unactiveContent}>Кому</li>
-              <NavLink
-                to="/products/to-friends"
-                className={classNames(styleForLink, "link")}
-              >
-                <li className={styles.content}>Друзьям</li>
-              </NavLink>
-              <NavLink
-                to="/products/to-relatives"
-                className={classNames(styleForLink, "link")}
-              >
-                <li className={styles.unactiveContent}>Близким</li>
-              </NavLink>
-              <NavLink
-                to="/products/to-just-merried"
-                className={classNames(styleForLink, "link")}
-              >
-                <li className={styles.content}>Молодожёнам</li>
-              </NavLink>
-              <div className={styles.separator}></div>
-              <li className={styles.unactiveContent}>...</li>
-              <NavLink
-                to="/products/wedding"
-                className={classNames(styleForLink, "link")}
-              >
-                <li className={styles.content}>Свадьба</li>
-              </NavLink>
-            </div>
-          </li>
-        </ul>
+                )}
+              </li>
+            </Tab>
+          </TabList>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+        </Tabs>
       </div>
     </>
   );
